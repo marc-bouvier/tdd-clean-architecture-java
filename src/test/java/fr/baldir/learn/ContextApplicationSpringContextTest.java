4dc0afd3f4225fa280f50f9d6be8204538168b09
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 22:25:34 +0100

    style: fix sonar

--- src/test/java/fr/baldir/learn/ContextApplicationSpringContextTest.java
+++ src/test/java/fr/baldir/learn/ContextApplicationSpringContextTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ContextApplicationSpringContextTest {
+ class ContextApplicationSpringContextTest {
 
+    @SuppressWarnings("java:S2699")
     @Test void out_of_the_box_spring_application_context_should_not_fail(){
-
+        //This test has no assertion, we want to see if spring context does not throw exception
     }
 }
