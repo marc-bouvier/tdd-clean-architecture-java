ad8d350c7ec24bc91ad7d78c6fd5274b78e52450
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 18:37:38 +0100

    ci: junit 4

--- src/main/java/fr/baldir/learn/Dummy.java
+++ src/main/java/fr/baldir/learn/Dummy.java
@@ -3,6 +3,6 @@ package fr.baldir.learn;
 public class Dummy {
 
     public void removeMe() {
-        throw new UnsupportedOperationException("I am not real! please remove me!");
+        System.out.println("Hello world!");
     }
 }
