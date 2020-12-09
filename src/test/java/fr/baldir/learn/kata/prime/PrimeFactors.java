70b253741b820214f8244d8f216bba4f03816dc2
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:27:01 +0100

    test nouveau test qui cible bien ce qu'on veut cibler

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
@@ -7,7 +7,7 @@ public class PrimeFactors {
 
     public List<Integer> generate(int n) {
         List<Integer> primeFactors = new ArrayList<>();
-
+        // SI ca devient trop compliqué c'est qu'on va pas dans la bonne direction
         if (n > 1) {
             primeFactors.add(n);
         }
