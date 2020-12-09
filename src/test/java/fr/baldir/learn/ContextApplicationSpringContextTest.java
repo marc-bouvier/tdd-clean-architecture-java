46ef6ef803142edea2388a6cee8839ff7597bb90
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:55:59 +0100

    test: noop spring application context test

---
+++ src/test/java/fr/baldir/learn/ContextApplicationSpringContextTest.java
@@ -0,0 +1,12 @@
+package fr.baldir.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContextApplicationSpringContextTest {
+
+    @Test void out_of_the_box_spring_application_context_should_not_fail(){
+
+    }
+}
