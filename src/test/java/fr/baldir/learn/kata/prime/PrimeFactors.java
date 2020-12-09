15679c329eca517fa2cfb644b942556f9e7ef5f8
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:38:24 +0100

    refactor ok

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
@@ -8,14 +8,12 @@ public class PrimeFactors {
     public List<Integer> generate(int n) {
         List<Integer> primeFactors = new ArrayList<>();
         int potentialDivisor = 2;
-        if (n != 1) {
             while (n!=1) {
                 while (n % potentialDivisor == 0) {
                     primeFactors.add(potentialDivisor);
                     n /= potentialDivisor;
                 }
                 potentialDivisor++;
-            }
 
         }
         return primeFactors;
