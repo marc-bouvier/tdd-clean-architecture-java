ba4ed31e939f78f6624a25c09c7f85cb82df1d10
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:29:17 +0100

    style clean comment

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
@@ -7,7 +7,6 @@ public class PrimeFactors {
 
     public List<Integer> generate(int n) {
         List<Integer> primeFactors = new ArrayList<>();
-        // SI ca devient trop compliqué c'est qu'on va pas dans la bonne direction
         if (n != 1) {
             if (n % 2 == 0) { //4
                 primeFactors.add(2); // 2 X
