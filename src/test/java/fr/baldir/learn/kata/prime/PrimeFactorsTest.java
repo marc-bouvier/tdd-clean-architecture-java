8e13cca777007fb3d3d399855e5cd8666df5c9a3
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:14:35 +0100

    test refactor given method

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactorsTest.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactorsTest.java
@@ -3,6 +3,7 @@ package fr.baldir.learn.kata.prime;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,7 +24,8 @@ public class PrimeFactorsTest {
 
     // IL évite les détails du quoi et des résultats dans les libellés de tests
     // Ex. on donne un titre générique au premier test
-    @Test void should_generate_the_prime_factors_of_a_given_number(){
+    @Test
+    void should_generate_the_prime_factors_of_a_given_number() {
         // Programming by wishful thinking
         // Ce que j'aimerais voir comme API
         // On se laisse guider par le compilateur et l'ide
@@ -33,17 +35,21 @@ public class PrimeFactorsTest {
         // Au lieu de 3 on pirrait tester 1. emptyList
         // C'est un degenerate case (cas subtil)
         // Le cas le plus simple
-        assertThat(new PrimeFactors().generate(1))
+        assertThat(generateFor(1))
                 .isEqualTo(Collections.emptyList());
 
         // Quel va être le 2eme test?
         // On veut faire un nouveau test rouge qui ajoute une fonctionalité
-        assertThat(new PrimeFactors().generate(2))
+        assertThat(generateFor(2))
                 .isEqualTo(Collections.singletonList(2));
         // Le but c'est d'aller le plus vite possible (ca peut etre crade
 
+        // Refactor test
 
 
+    }
 
+    private List<Integer> generateFor(int i) {
+        return new PrimeFactors().generate(i);
     }
 }
