7084630e6f4d0328726240972f67c571705f4d9a
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:28:11 +0100

    test baby step

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
@@ -8,8 +8,13 @@ public class PrimeFactors {
     public List<Integer> generate(int n) {
         List<Integer> primeFactors = new ArrayList<>();
         // SI ca devient trop compliqué c'est qu'on va pas dans la bonne direction
-        if (n > 1) {
-            primeFactors.add(n);
+        if (n != 1) {
+            if (n % 2 == 0) {
+                primeFactors.add(2);
+                n /= 2;
+            }
+            if (n > 1)
+                primeFactors.add(n);
         }
         return primeFactors;
     }
