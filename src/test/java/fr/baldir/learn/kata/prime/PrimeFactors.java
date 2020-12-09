758cdb71af76b75afad2b6837d7ad8cfb94325e0
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-09 20:22:40 +0100

    refactor generalize

--- src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
+++ src/test/java/fr/baldir/learn/kata/prime/PrimeFactors.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class PrimeFactors {
 
-
     public List<Integer> generate(int n) {
         List<Integer> primeFactors = new ArrayList<>();
+        // On est passé d'une constante à une variable
         if (n > 1) {
             primeFactors.add(n);
         }
