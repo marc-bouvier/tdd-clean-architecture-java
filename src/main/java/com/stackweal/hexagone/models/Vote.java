235f8d53fe127c9f486e2539c41609caa33d2cee
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-12-10 00:51:51 +0100

    refactor: lombok for equals and hash code

--- src/main/java/com/stackweal/hexagone/models/Vote.java
+++ src/main/java/com/stackweal/hexagone/models/Vote.java
@@ -1,12 +1,14 @@
 package com.stackweal.hexagone.models;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
+@EqualsAndHashCode
 public class Vote {
     private int i;
-    private String answerId;
+    private final String answerId;
 
-    public Vote(String answerId, int i) {
+    public Vote(@NonNull String answerId, int i) {
         this.answerId = answerId;
         this.i = i;
     }
@@ -15,18 +17,5 @@ public class Vote {
         this.i += 1;
     }
 
-    @Override
-    public boolean equals(Object o) {
 
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Vote vote = (Vote) o;
-        return i == vote.i &&
-                Objects.equals(answerId, vote.answerId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(i, answerId);
-    }
 }
